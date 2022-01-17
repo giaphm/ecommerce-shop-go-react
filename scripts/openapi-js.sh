67d109d362a6c7c347f2a57e474779573c8a602d
giaphm <minhgiaphoang1997@gmail.com>
2022-01-17 13:21:06 +0700

    Fixing openapi-js.sh permission denied

--- scripts/openapi-js.sh
+++ scripts/openapi-js.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for service in trainer trainings users; do
+for service in checkouts orders products users; do
     /usr/local/bin/docker-entrypoint.sh generate \
         -i "./api/openapi/$service.yml" \
         -g javascript \
