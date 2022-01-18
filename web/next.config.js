a96434dad7c0dba7fe1fde6a8b70a797badb9bd7
giaphm <minhgiaphoang1997@gmail.com>
2022-01-18 13:03:20 +0700

    update next.config.js: Add exportPathMap

--- web/next.config.js
+++ web/next.config.js
@@ -8,4 +8,17 @@ module.exports = {
     }
     return config
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/login': { page: '/login' },
+      '/signup': { page: '/signup' },
+      // '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+      // '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+      // '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+    }
+  },
 }
